1af2e8643eb38c7149a9becb1080e788bac9dcaa
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-06-02 19:59:20 +0300

    CS-13 upload category schema

---
+++ models/categorySchema.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose");
+
+const schema = new mongoose.Schema({
+    _id:{
+        type: mongoose.Schema.Types.ObjectId,
+        auto: true
+      },
+    name:{
+        type:String,
+        required:true
+    },
+    products_id:{
+        type:[mongoose.Schema.Types.ObjectId],
+        ref:'products',
+        required:true
+    },
+    image:{
+        type:String,
+        required: true
+    },
+})
+
+mongoose.model("categories",schema);
