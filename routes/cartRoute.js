f946eb75b139b46297c1bb0c1555c6563eb152ff
aya <aya.saeed126@gmail.com>
2023-06-05 18:57:44 +0300

    CS-10 update cart route

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -12,8 +12,8 @@ router
   .patch(validations.updateProduct,validator, controller.updateProductInCart)
   .delete(validations.deleteProduct,validator, controller.deleteProductFromCart);
 
-router.route("/:id").get(validations.cartId,validator, controller.getCartById);
+router.route("/cart/:id").get(validations.cartId,validator, controller.getCartById);
 
-router.route("/empty").patch(validations.emptyCart,validator, controller.emptyCart);
+router.route("cart/empty").patch(validations.emptyCart,validator, controller.emptyCart);
 
 module.exports = router;
