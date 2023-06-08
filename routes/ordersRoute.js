8315c759798337c87e9cf074964478bfdfa9c77f
Amalhalim <amalhalim333@gmail.com>
2023-06-08 23:27:26 +0300

    modification of order control and add authontication

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -8,11 +8,11 @@ const Router = express.Router();
 
 
 Router.route("/orders")
-  .get(controller.getAllOrders)
-  .post(OrderValidation.POSTValidation,validator, controller.postOrders)
-  .patch(OrderValidation.UpdateValidation,validator,controller.updateSingleOrders)
-  .delete(OrderValidation.deleteValidation,validator,controller.deleteSingleOrders);
+  .get(isAdmin, controller.getAllOrders)
+  .post(isUserOrAdmin,OrderValidation.POSTValidation,validator, controller.postOrders)
+  // .patch(isUserOfIdOrAdmin,OrderValidation.UpdateValidation,validator,controller.updateSingleOrders)
+  .delete(isUserOfIdOrAdmin,OrderValidation.deleteValidation,validator,controller.deleteSingleOrders);
 
-Router.route("/orders/:id").get(OrderValidation.IdValidation,validator,controller.getSingleOrders);
+Router.route("/orders/:id").get(isUserOfIdOrAdmin,OrderValidation.IdValidation,validator,controller.getSingleOrders);
 
 module.exports = Router;
