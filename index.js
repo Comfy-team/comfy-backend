0c32582c113ee15aea6607b9cd9bcce82510e192
aya <aya.saeed126@gmail.com>
2023-06-08 18:18:22 +0300

    CS-31 uncommented auth and login routes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const brandRoutes = require("./routes/brandRoute");
 const categoryRoutes = require("./routes/categoryRoute");
 const productRoutes = require("./routes/productRoutes");
 
-const authMW = require("./middlewares/authMW");
+const authMW = require("./middlewares/authMw");
 
 const port = process.env.port || 8080;
 const server = express();
@@ -36,10 +36,10 @@ server.use(morgan("short"));
 server.use(express.json());
 
 // authentication
-// server.use(authenticationRoute);
+server.use(authenticationRoute);
 
 // authorization
-// server.use(authMW);
+server.use(authMW);
 
 // routes
 server.use(productRoutes);
