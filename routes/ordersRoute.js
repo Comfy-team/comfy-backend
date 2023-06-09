6b582e94db316e34027e228340357a317722e5b7
Amalhalim <amalhalim333@gmail.com>
2023-06-09 19:30:29 +0300

    modify authorization in order route

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -3,16 +3,19 @@ const express = require("express");
 const validator = require("../middlewares/validations/validator");
 const OrderValidation = require("../middlewares/validations/ordersValidation");
 const controller = require("../controllers/ordersController");
+const { isAdmin } = require("../middlewares/authMw");
+const { isUserOrAdmin } = require("../middlewares/authMw");
+const { isUserOfIdOrAdmin } = require("../middlewares/authMw");
+const authMW =require("./../middlewares/authMw");
 
 const Router = express.Router();
 
 
 Router.route("/orders")
-  .get(isAdmin, controller.getAllOrders)
-  .post(isUserOrAdmin,OrderValidation.POSTValidation,validator, controller.postOrders)
-  // .patch(isUserOfIdOrAdmin,OrderValidation.UpdateValidation,validator,controller.updateSingleOrders)
-  .delete(isUserOfIdOrAdmin,OrderValidation.deleteValidation,validator,controller.deleteSingleOrders);
+  .get(authMW.verifyToken,isAdmin, controller.getAllOrders)
+  .post(authMW.verifyToken,isUserOrAdmin,OrderValidation.POSTValidation,validator, controller.postOrders)
+  .delete(authMW.verifyToken,isUserOfIdOrAdmin,OrderValidation.deleteValidation,validator,controller.deleteSingleOrders);
 
-Router.route("/orders/:id").get(isUserOfIdOrAdmin,OrderValidation.IdValidation,validator,controller.getSingleOrders);
+Router.route("/orders/:id").get(authMW.verifyToken,isUserOfIdOrAdmin,OrderValidation.IdValidation,validator,controller.getSingleOrders);
 
 module.exports = Router;
