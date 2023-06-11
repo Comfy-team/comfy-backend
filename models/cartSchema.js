d9ab208aec4dd9ed27c362f7ac3361017e7f6939
mariam-abdallah <mariam.m.r.ali@gmail.com>
2023-06-11 18:56:54 +0300

    upload cartSchema

---
+++ models/cartSchema.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const cartItemSchema = new mongoose.Schema({
+  product_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "products",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "products"
+  },
+  
+});
+
+const schema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    default: 0
+  },
+  items: {
+    type: [cartItemSchema],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("cart", schema);
