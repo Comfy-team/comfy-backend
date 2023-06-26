44734e5cf6beaad22a4014bb4148074159428b0b
mariam-abdallah <mariam.m.r.ali@gmail.com>
2023-06-27 01:02:12 +0300

    CS-14 handle pagination

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,63 +1,70 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 require("../models/categorySchema");
 
-const  Category= mongoose.model("categories");
+const Category = mongoose.model("categories");
 
-module.exports.getAllCategory = (request,response,next) => 
-{
+module.exports.getAllCategory = (request, response, next) => {
     Category.find({})
-      .then((data) => response.status(200).json(data))
-      .catch((error) => next(error));
+        .then((data) => {
+            // handle pagination
+            const page = req.query.page ? req.query.page : 1;
+            const { totalPages, pageData } = getDataOfPage(data, page);
+            response.status(200).json({
+                data: pageData,
+                totalPages,
+                totalCategories: data.length,
+            });
+        })
+        .catch((error) => next(error));
 };
 
 
-module.exports.getCategoryById = (request,response,next) => {
+module.exports.getCategoryById = (request, response, next) => {
 
     Category.findOne({ _id: request.params.id })
-    .then((obj) => {
-      if (obj === null) {
-        throw new Error("Category isn't found");
-      }
-      response.status(200).json(obj);
-    })
-    .catch((error) => next(error));
+        .then((obj) => {
+            if (obj === null) {
+                throw new Error("Category isn't found");
+            }
+            response.status(200).json(obj);
+        })
+        .catch((error) => next(error));
 }
 
-module.exports.addCategory = (request,response,next) => {
+module.exports.addCategory = (request, response, next) => {
     let object = new Category({
         name: request.body.name,
-        image:request.file.path
-      });
-      object.save()
-      .then(data=>{
-          response.status(201).json(data)
-      }).catch(error=>next(error));
+        image: request.file.path
+    });
+    object.save()
+        .then(data => {
+            response.status(201).json(data)
+        }).catch(error => next(error));
 }
 
-module.exports.updateCategory = (request,response,next) => {
+module.exports.updateCategory = (request, response, next) => {
     const imagePath = request.file ? request.file.path : request.data;
-    Category.updateOne({_id:request.body.id},
+    Category.updateOne({ _id: request.body.id },
         {
-            $set:{
+            $set: {
                 name: request.body.name,
-                image:imagePath
+                image: imagePath
             }
         })
-        .then((data) => 
-        {
-          response.status(200).json(data);
+        .then((data) => {
+            response.status(200).json(data);
         }).catch((error) => next(error))
 }
 
-module.exports.deleteCategory = (request,response,next) => {
-     Category.findOne({ _id: request.body.id})
-     .then((category)=>{
-        if(!category){
-           throw new Error('Category not found with the specified _id value');
-        }
-        return Category.deleteOne({ _id: request.body.id })
-    }).then(data=>{
-        response.status(200).json(data);
-    }).catch(error=>next(error));
+module.exports.deleteCategory = (request, response, next) => {
+    Category.findOne({ _id: request.body.id })
+        .then((category) => {
+            if (!category) {
+                throw new Error('Category not found with the specified _id value');
+            }
+            return Category.deleteOne({ _id: request.body.id })
+        }).then(data => {
+            response.status(200).json(data);
+        }).catch(error => next(error));
 
 }
