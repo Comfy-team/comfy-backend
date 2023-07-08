750a9bf719a10760ccb8cd41bf2603b03c62b814
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-07-09 00:03:02 +0300

    CS-17 update user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -135,7 +135,7 @@ module.exports.getUserCart = (request, response, next) => {
       path: "cart_id",
       populate: {
         path: "items.product_id",
-        select: { name: 1, images: 1, brand: 1, stock:1,discount:1 },
+        select: { name: 1, images: 1, brand: 1, price:1,discount:1 },
         populate: {
           path: "brand",
           select: { name: 1 },
