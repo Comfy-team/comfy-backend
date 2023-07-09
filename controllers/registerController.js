fab3e8a6bdf691791d444535b09190ade746ce20
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-07-09 22:53:34 +0300

    CS-38 update register controller

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,35 +1,39 @@
 const mongoose = require("mongoose");
 
 const bcrypt = require("bcrypt");
-const saltRounds = +process.env.saltRounds
+const saltRounds = +process.env.saltRounds;
 const salt = bcrypt.genSaltSync(saltRounds);
 
 const User = mongoose.model("users");
-const Cart = mongoose.model('cart');
+const Cart = mongoose.model("cart");
 
-module.exports.register=(request,response,next)=>{
-    let object = new User({
-        fullName: request.body.fullName,
-        password: bcrypt.hashSync(request.body.password, salt),
-        email: request.body.email,
-        phone: request.body.phone || "",
-        address:  request.body.address,
-      })
+module.exports.register = (request, response, next) => {
+  let object = new User({
+    fullName: request.body.fullName,
+    password: bcrypt.hashSync(request.body.password, salt),
+    email: request.body.email,
+    phone: request.body.phone || "",
+    address: request.body.address,
+  });
 
-    object.save().then(data=>{
-        let cartObj=new Cart({
-            user_id:data._id,
-            totalPrice:0,
-            items:[]
-        });
-        return cartObj.save();
-        
-    }).then(async (data)=>{
-        await User.updateOne({_id:data.user_id},{$set:{cart_id: data._id}})
-        return User.findOne({_id:data.user_id})
-    }).then(data=>{
-        response.status(201).json(data);
+  object
+    .save()
+    .then((data) => {
+      let cartObj = new Cart({
+        user_id: data._id,
+        items: [],
+      });
+      return cartObj.save();
     })
-    .catch(error=>next(error))
-
-}
+    .then(async (data) => {
+      await User.updateOne(
+        { _id: data.user_id },
+        { $set: { cart_id: data._id } }
+      );
+      return User.findOne({ _id: data.user_id });
+    })
+    .then((data) => {
+      response.status(201).json(data);
+    })
+    .catch((error) => next(error));
+};
